541c43f4fbcc5f5ac5b2d3bbe17830772d82dda4
Om Gupta <86357151+AumGupta@users.noreply.github.com>
2023-06-21 20:02:31 +0530

    Report List Screen Added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 Color darkPurple = const Color(0xFF343965);
-Color primaryColor = const Color(0xff585be4);
-Color primaryColorTransparent = const Color(0x90585be4);
 Color blueTint = const Color(0xFFDEE8FD);
 
-Color screenBgColor = const Color(0xFFFAFBFF);
+Color primaryColor = const Color(0xff585be4);
+Color primaryColorTransparent = const Color(0x90585be4);
+
 Color whiteColorTransparent = const Color(0x26FFFFFF);
+
+Color screenBgColor = const Color(0xFFFAFBFF);
 Color greyColor = const Color(0xFFA1A6C0);
 
 Color lightGreenColor = const Color(0xFFcdecea);
